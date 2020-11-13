79ec16707dea56b4e46adfb39ffb9c26bedd7e5e
victor hugo souza vilela <victorsouvilela@hotmail.com>
2020-11-12 21:42:10 -0300

    Finalizando o projeto

---
+++ app/src/main/java/com/example/frasesdodia/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.frasesdodia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var texto:TextView
+    val frases= arrayOf(
+        "O importante não é vencer todos os dias, mas lutar sempre",
+        "Maior que a tristeza de não haver vencido, é a vergonha de não ter lutado",
+        "É melhor conquistar a si mesmo do que vencer mil batalhas",
+        "Enquanto houver vontade de lutar havera esperança de vencer"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        texto=findViewById(R.id.textoFrase) as TextView
+
+    }
+
+        fun gerarFrases(view:View){
+
+            val totalItensArray=frases.size
+            val numeroAleatorio = Random().nextInt(totalItensArray)
+
+            texto.setText(frases[numeroAleatorio])
+        }
+
+
+}
